060dba93889dc13d6c99f03a9f1f993fbd0a3d01
Gantyuk Vyacheslav <Vgantyuk2@gmail.com>
2017-01-03 00:06:28 +0200

    task3

--- header.php
+++ header.php
@@ -15,8 +15,8 @@
 		<div class="sidebar">
 			<ul>
 				<li><a href="task1.php">Завдання 1</a></li>
-				<li><a href="file1.php">Завдання 2</a></li>
-				<li><a href="file1.php">Завдання 3</a></li>
+				<li><a href="task2.php">Завдання 2</a></li>
+				<li><a href="task3.php">Завдання 3</a></li>
 				<li><a href="file1.php">Завдання 4</a></li>
 				<li><a href="file1.php">Завдання 5</a></li>
 				<li><a href="file1.php">Завдання 6</a></li>			
