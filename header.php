d86470b8065f74aa9623a6fcfe0dfc4831634eee
Gantyuk Vyacheslav <Vgantyuk2@gmail.com>
2017-01-02 20:07:44 +0200

    first comit

---
+++ header.php
@@ -0,0 +1,25 @@
+<title>SoftGroup</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+	<div class="header">
+		<img id="logo" src="Logo.png"/>
+		<div class="menu_head">
+			<a href="index.php">Головна  </a> 
+			<a href="file1.php">Автор </a> 
+			<a target="_blank" href="http://www.soft-group.com">SoftGroup </a>
+		</div>
+		<div class="text_head"><h2>SoftGroup </h2> <h3>Тестове завдання</h3></div>
+	</div>
+	<div class="pages">
+		<div class="sidebar">
+			<ul>
+				<li><a href="file1.php">Завдання 1</a></li>
+				<li><a href="file1.php">Завдання 2</a></li>
+				<li><a href="file1.php">Завдання 3</a></li>
+				<li><a href="file1.php">Завдання 4</a></li>
+				<li><a href="file1.php">Завдання 5</a></li>
+				<li><a href="file1.php">Завдання 6</a></li>			
+			</ul>
+
+		</div>
